fdcd3aa3831f55068e492e6252c17d7801b56bce
GamerSh <93838417+GamerShabandari@users.noreply.github.com>
2021-11-22 15:09:25 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+// ta in användarens input ålder
+
+let minAlder = document.getElementById("minAlder");
+
+
+
+// skapa knapp som vid klick kollar rätt ålder
+
+
+//om ålder är 18 och upp tillräckligt gammal
+
+
+// om ålder är under 18 för ung
+
